c344ff3ae3e872dfe5e15d2548b1b903233d159b
pgeeganage <pgeeganage@wiley.com>
2024-03-28 15:10:22 +0530

    extract common code to utility class

---
+++ src/main/java/org/example/utility/Utility.java
@@ -0,0 +1,14 @@
+package org.example.utility;
+
+public final class Utility {
+
+    public static final String[] EXPECTED_IMPORT_ORDER = {
+            "import java.",
+            "import javax.",
+            "import org.",
+            "import com.",
+            "import static org.",
+            "import static com.",
+            "import ccp.",
+            "import lombok."};
+}
